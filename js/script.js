2ed94a2dae0088417a02cf1ce09f777b37ab5720
Stas Kopytich <35845706+kopytich2704@users.noreply.github.com>
2021-11-06 19:01:21 +0300

    Fixed the navbar issues

---
+++ js/script.js
@@ -0,0 +1,14 @@
+AOS.init(); // AOS Initialization
+
+
+// Runs the hamburger animation
+let wrapperMenu = document.querySelector('.nav-burger');
+
+wrapperMenu.addEventListener('click', function () {
+    wrapperMenu.classList.toggle('nav-burger_open');
+});
+
+// Closes and opens the navbar menu
+document.getElementById('nav-burger').onclick = function () {
+    document.getElementById('nav-menu').classList.toggle('nav-menu_displayed');
+};
